64f05aed0e9a34a66de8789b0150781a6425f219
Xinwei Xie <xenway@gmail.com>
2018-11-02 12:06:49 +1100

    using JPA entities

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,64 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+
+    }
+
+    public Author(String fn, String ln) {
+        this.firstName = fn;
+        this.lastName = ln;
+    }
+
+    public Author(String fn, String ln, Set<Book> b) {
+        this.firstName = fn;
+        this.lastName = ln;
+        this.books = b;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
