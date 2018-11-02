81028f3aa0c400fc7adac3e549cee11c73aa8ee7
Xinwei Xie <xenway@gmail.com>
2018-11-02 14:24:21 +1100

    add Publisher Entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,28 +15,36 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         initData();
     }
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData() {
         Author eric = new Author("Eric", "Evans");
 
-        Book b = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher p1 = new Publisher("Harper Collins", "New York");
+        Publisher p2 = new Publisher("Worx", "Los Angles");
+        publisherRepository.save(p1);
+        publisherRepository.save(p2);
+
+        Book b = new Book("Domain Driven Design", "1234", p1);
         eric.getBooks().add(b);
         b.getAuthors().add(eric);
         authorRepository.save(eric);
         bookRepository.save(b);
 
+
         Author rod = new Author("Rod", "Johnson");
-        Book b2 = new Book("J2EE Development", "2345", "Worx");
+        Book b2 = new Book("J2EE Development", "2345", p2);
         rod.getBooks().add(b2);
         b2.getAuthors().add(rod);
         authorRepository.save(rod);
